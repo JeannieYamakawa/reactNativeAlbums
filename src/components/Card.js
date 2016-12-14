230d76875668ca20f4a9a7f3bd0393891d7f66fd
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-14 00:36:19 -0600

    styling for header/card section

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => {
-    return (<View style={styles.containerStyle}>
-
-    </View>
+const Card = (props) => {
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
     );
 };
 
